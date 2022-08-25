0749bc5c81c58036c5830d23fc51c6107c21f856
Simon <gevhaz@tutanota.com>
2022-08-25 23:48:31 +0200

    Test print_all and finish testing print_metadata
    The functions print_all and print_metadata are fully tested and older
    tests have been adjusted after fixing a bug in the ANSI color codes in
    the actual printing function.

--- tests/test_music_tags.py
+++ tests/test_music_tags.py
@@ -1,4 +1,5 @@
 """Tests for music_tags.py."""
+import sys
 import unittest
 
 import pytest
@@ -51,12 +52,68 @@ class TestUtils(unittest.TestCase):
 
         self.assertEqual(expected, captured.out)
 
+    def test_print_metadata_with_performers(self) -> None:
+        """Test printing of metadata where there are performer tags."""
+        tags: Tags = {"artist": ["artist 1", "artist 2"], "performer:vocals": ["bassist 1"]}
+        MusicTags.print_metadata(tags, Fore.RED)
+        captured = self.capsys.readouterr()  # type: ignore
+        expected = (
+            "  Performers:\n"
+            f"  - Vocals: {Fore.RED}bassist 1{Fore.RESET}\n"
+            f"  Title: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Album: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Album Artist: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Artist(s): {Fore.RED}artist 1 | artist 2{Fore.RESET}\n"
+            f"  Date: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Genre: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Version: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Performer: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Organization: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Copyright: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Composer: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Conductor: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Arranger: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Author: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Producer: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Publisher: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Lyricist: {Fore.BLACK}Not found{Fore.RESET}\n\n"
+        )
+
+        self.assertEqual(expected, captured.out)
+
+    def test_print_metadata_with_unknown_performer_tag(self) -> None:
+        """Test printing of metadata with unknown performer tag."""
+        tags: Tags = {"artist": ["artist 1", "artist 2"], "performer:unknown": ["person 1"]}
+        MusicTags.print_metadata(tags, Fore.RED)
+        captured = self.capsys.readouterr()  # type: ignore
+        expected = (
+            f"  Title: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Album: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Album Artist: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Artist(s): {Fore.RED}artist 1 | artist 2{Fore.RESET}\n"
+            f"  Date: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Genre: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Version: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Performer: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Organization: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Copyright: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Composer: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Conductor: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Arranger: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Author: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Producer: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Publisher: {Fore.BLACK}Not found{Fore.RESET}\n"
+            f"  Lyricist: {Fore.BLACK}Not found{Fore.RESET}\n\n"
+        )
+
+        self.assertEqual(expected, captured.out)
+
     def test_get_tag_data_one_source(self) -> None:
         """Test getting tags from one source."""
         tags = MusicTags()
         tags.original = {"artist": ["artist 1"]}
         actual = tags.get_tag_data("artist")
-        expected = [f"{Fore.CYAN}artist 1"]
+        expected = [f"{Fore.CYAN}artist 1{Fore.RESET}"]
         self.assertListEqual(expected, actual)
 
     def test_get_tag_data_two_sources(self) -> None:
@@ -65,7 +122,7 @@ class TestUtils(unittest.TestCase):
         tags.original = {"artist": ["artist 1"]}
         tags.youtube = {"artist": ["artist 2"]}
         actual = tags.get_tag_data("artist")
-        expected = [f"{Fore.CYAN}artist 1", f"{Fore.MAGENTA}artist 2"]
+        expected = [f"{Fore.CYAN}artist 1{Fore.RESET}", f"{Fore.MAGENTA}artist 2{Fore.RESET}"]
         self.assertListEqual(expected, actual)
 
     def test_get_tag_data_two_overlapping_sources(self) -> None:
@@ -74,7 +131,7 @@ class TestUtils(unittest.TestCase):
         tags.original = {"artist": ["artist 1"]}
         tags.youtube = {"artist": ["artist 1", "artist 2"]}
         actual = tags.get_tag_data("artist")
-        expected = [f"{Fore.CYAN}artist 1", f"{Fore.MAGENTA}artist 1 | artist 2"]
+        expected = [f"{Fore.CYAN}artist 1{Fore.RESET}", f"{Fore.MAGENTA}artist 1 | artist 2{Fore.RESET}"]
         self.assertListEqual(expected, actual)
 
     def test_get_tag_data_no_values(self) -> None:
@@ -93,9 +150,91 @@ class TestUtils(unittest.TestCase):
         tags.fromdesc = {"artist": ["artist 4"]}
         actual = tags.get_tag_data("artist")
         expected = [
-            f"{Fore.CYAN}artist 1",
-            f"{Fore.MAGENTA}artist 2",
-            f"{Fore.YELLOW}artist 3",
-            f"{Fore.GREEN}artist 4",
+            f"{Fore.CYAN}artist 1{Fore.RESET}",
+            f"{Fore.MAGENTA}artist 2{Fore.RESET}",
+            f"{Fore.YELLOW}artist 3{Fore.RESET}",
+            f"{Fore.GREEN}artist 4{Fore.RESET}",
         ]
         self.assertListEqual(expected, actual)
+
+    def test_print_all(self) -> None:
+        """Test printing the combined metadata of all sources."""
+        tags = MusicTags()
+        tags.original = {"artist": ["artist 1"]}
+        tags.youtube = {"artist": ["artist 2"]}
+        tags.fromtags = {"artist": ["artist 3"]}
+        tags.fromdesc = {"artist": ["artist 4"]}
+        tags.print_all()
+        captured = self.capsys.readouterr()  # type: ignore
+        expected = (
+            f"Title: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Album: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Album Artist: {Fore.BLACK}Not set{Fore.RESET}\n"
+            "Artist(s): "
+            f"{Fore.CYAN}artist 1{Fore.RESET} | "
+            f"{Fore.MAGENTA}artist 2{Fore.RESET} | "
+            f"{Fore.YELLOW}artist 3{Fore.RESET} | "
+            f"{Fore.GREEN}artist 4{Fore.RESET}\n"
+            f"Date: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Genre: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Version: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Performer: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Organization: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Copyright: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Composer: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Conductor: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Arranger: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Author: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Producer: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Publisher: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Lyricist: {Fore.BLACK}Not set{Fore.RESET}\n\n"
+        )
+        self.assertEqual(expected, captured.out)
+
+    def test_print_all_with_performers(self) -> None:
+        """Test printing combined data when there are performer tags."""
+        tags = MusicTags()
+        tags.original = {"artist": ["artist 1"]}
+        tags.youtube = {"artist": ["artist 2"]}
+        tags.fromtags = {"artist": ["artist 3"]}
+        tags.fromdesc = {"performer:vocals": ["artist 4"]}
+        tags.print_all()
+        captured = self.capsys.readouterr()  # type: ignore
+        expected = (
+            "Performers:\n"
+            f"- Vocals: {Fore.GREEN}artist 4{Fore.RESET}\n"
+            f"Title: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Album: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Album Artist: {Fore.BLACK}Not set{Fore.RESET}\n"
+            "Artist(s): "
+            f"{Fore.CYAN}artist 1{Fore.RESET} | "
+            f"{Fore.MAGENTA}artist 2{Fore.RESET} | "
+            f"{Fore.YELLOW}artist 3{Fore.RESET}\n"
+            f"Date: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Genre: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Version: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Performer: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Organization: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Copyright: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Composer: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Conductor: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Arranger: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Author: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Producer: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Publisher: {Fore.BLACK}Not set{Fore.RESET}\n"
+            f"Lyricist: {Fore.BLACK}Not set{Fore.RESET}\n\n"
+        )
+        print("------")
+        sys.stdout.write(captured.out)
+        print("------")
+        self.assertEqual(expected, captured.out)
+
+    def test_print_all_with_no_data(self) -> None:
+        """Test printing the combined metadata of all sources."""
+        tags = MusicTags()
+        tags.print_all()
+        captured = self.capsys.readouterr()  # type: ignore
+        expected = (
+            f"{Fore.RED}There's no data to be printed{Fore.RESET}\n"
+        )
+        self.assertEqual(expected, captured.out)
